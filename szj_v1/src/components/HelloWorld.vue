d103fa58d755a043ef31c7edb2d55ed6002e2173
shenziji <shenziji@yiqing.com>
2020-11-25 11:47:21 +0800

    1

---
+++ szj_v1/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+     <div class="text">
+       <span>123</span>
+     </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang = "less" scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.hello {
+  .text {
+    span {
+      color: #ff0000;
+    }
+  }
+}
+</style>
